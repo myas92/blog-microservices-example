f37672552dcf5cdfe891d2442dc761bec111512d
Yaser Ahmadi <m.y.ahmadi22@gmail.com>
2022-08-20 00:00:40 +0430

    init posts

--- posts/index.js
+++ posts/index.js
@@ -1,2 +1,23 @@
 const epxress = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
 const app = epxress();
+app.use(bodyParser.json())
+const posts = {};
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    let id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    }
+    res.status(201).send(posts[id])
+})
+
+
+app.listen(4000, ()=>{
+    console.log('Listening on 4000');
+})
