58ab94e3bbb979106705197821f9094cc788fc67
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-08-20 12:12:25 +0430

    init comments

--- comments/index.js
+++ comments/index.js
@@ -1,2 +1,25 @@
 const epxress = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 const app = epxress();
+const commentsByPostId = {};
+app.use(bodyParser.json())
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const { content } = req.body;
+    const commentId = randomBytes(4).toString("hex");
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content })
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments)
+})
+
+
+app.listen(4005, () => {
+    console.log('Listening on 4005')
+})
